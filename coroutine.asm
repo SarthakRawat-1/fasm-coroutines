095ab557c54a5c7df2446d57fc81b86c3fb8cce0
SarthakRawat-1 <sarthakrawat525@gmail.com>
2025-03-31 10:47:54 +0530

    Added Comments

--- coroutine.asm
+++ coroutine.asm
@@ -1,5 +1,6 @@
 format ELF64
 
+; System call numbers and file descriptors
 STDIN_FILENO = 0
 STDOUT_FILENO = 1
 STDERR_FILENO = 2
@@ -7,74 +8,87 @@ STDERR_FILENO = 2
 SYS_write = 1
 SYS_exit = 60
 
-COROUTINES_CAPACITY = 10
-STACK_CAPACITY = 4*1024
+; Configuration constants
+COROUTINES_CAPACITY = 10          ; Maximum number of coroutines
+STACK_CAPACITY = 4*1024          ; Size of stack for each coroutine (4KB)
 
 ;; TODO: make coroutine.o relocatable
 
 section '.text'
-;; rdi - procedure to start in a new coroutine
+;; Creates a new coroutine that starts executing the given procedure
+;; Input: rdi - procedure address to start in a new coroutine
 public coroutine_go
 coroutine_go:
+    ; Check if we haven't exceeded maximum coroutines
     cmp QWORD [contexts_count], COROUTINES_CAPACITY
     jge overflow_fail
 
-    mov rbx, [contexts_count]     ;; rbx contains the index of the
-                                  ;; context we just allocated
+    ; Get new coroutine index and increment count
+    mov rbx, [contexts_count]     ; rbx contains the index of the
+                                 ; context we just allocated
     inc QWORD [contexts_count]
 
-    mov rax, [stacks_end]         ;; rax contains the rsp of the new
-                                  ;; routine
+    ; Setup new stack for the coroutine
+    mov rax, [stacks_end]        ; rax contains the rsp of the new
+                                 ; routine
     sub QWORD [stacks_end], STACK_CAPACITY
     sub rax, 8
-    mov QWORD [rax], coroutine_finish
+    mov QWORD [rax], coroutine_finish  ; Push return address for when coroutine ends
 
-    mov [contexts_rsp+rbx*8], rax
-    mov QWORD [contexts_rbp+rbx*8], 0
-    mov [contexts_rip+rbx*8], rdi
+    ; Save coroutine context
+    mov [contexts_rsp+rbx*8], rax      ; Save stack pointer
+    mov QWORD [contexts_rbp+rbx*8], 0  ; Initialize base pointer
+    mov [contexts_rip+rbx*8], rdi      ; Save instruction pointer (procedure address)
 
     ret
 
-;; ...[ret]
-;;         ^
+;; Initializes the first coroutine (usually the main function)
+;; Must be called before any other coroutine operations
 public coroutine_init
 coroutine_init:
+    ; Check if we haven't exceeded maximum coroutines
     cmp QWORD [contexts_count], COROUTINES_CAPACITY
     jge overflow_fail
 
-    mov rbx, [contexts_count]     ;; rbx contains the index of the
-                                  ;; context we just allocated
+    ; Get new coroutine index and increment count
+    mov rbx, [contexts_count]     
     inc QWORD [contexts_count]
 
-    pop rax                       ;; return address is in rax now
-    mov [contexts_rsp+rbx*8], rsp
-    mov [contexts_rbp+rbx*8], rbp
-    mov [contexts_rip+rbx*8], rax
+    ; Save current execution context
+    pop rax                       ; Get return address
+    mov [contexts_rsp+rbx*8], rsp ; Save stack pointer
+    mov [contexts_rbp+rbx*8], rbp ; Save base pointer
+    mov [contexts_rip+rbx*8], rax ; Save instruction pointer (return address)
 
-    jmp rax
+    jmp rax                      ; Return to caller
 
-;; ...[ret]
-;;         ^
+;; Switches execution to the next coroutine in round-robin fashion
 public coroutine_yield
 coroutine_yield:
+    ; Get current coroutine index
     mov rbx, [contexts_current]
 
-    pop rax                       ;; return address is in rax now
-    mov [contexts_rsp+rbx*8], rsp
-    mov [contexts_rbp+rbx*8], rbp
-    mov [contexts_rip+rbx*8], rax
+    ; Save current execution context
+    pop rax                       ; Get return address
+    mov [contexts_rsp+rbx*8], rsp ; Save stack pointer
+    mov [contexts_rbp+rbx*8], rbp ; Save base pointer
+    mov [contexts_rip+rbx*8], rax ; Save instruction pointer
 
+    ; Move to next coroutine (with wraparound)
     inc rbx
-    xor rcx, rcx
+    xor rcx, rcx                 ; rcx = 0
     cmp rbx, [contexts_count]
-    cmovge rbx, rcx
+    cmovge rbx, rcx             ; If rbx >= contexts_count, rbx = 0
     mov [contexts_current], rbx
 
-    mov rsp, [contexts_rsp+rbx*8]
-    mov rbp, [contexts_rbp+rbx*8]
-    jmp QWORD [contexts_rip+rbx*8]
+    ; Restore next coroutine's context
+    mov rsp, [contexts_rsp+rbx*8] ; Restore stack pointer
+    mov rbp, [contexts_rbp+rbx*8] ; Restore base pointer
+    jmp QWORD [contexts_rip+rbx*8] ; Jump to saved instruction pointer
 
+;; Called when a coroutine function returns
 coroutine_finish:
+    ; Print error message (TODO: implement proper cleanup)
     mov rax, SYS_write
     mov rdi, STDERR_FILENO
     mov rsi, coroutine_finish_not_implemented
@@ -85,6 +99,7 @@ coroutine_finish:
     mov rdi, 69
     syscall
 
+;; Error handler for too many coroutines
 overflow_fail:
     mov rax, SYS_write
     mov rdi, STDERR_FILENO
@@ -97,6 +112,7 @@ overflow_fail:
     syscall
 
 section '.data'
+; Error messages and other static data
 too_many_coroutines_msg: db "ERROR: Too many coroutines", 0, 10
 too_many_coroutines_msg_len = $-too_many_coroutines_msg
 coroutine_finish_not_implemented: db "TODO: coroutine_finish is not implemented", 0, 10
@@ -104,13 +120,17 @@ coroutine_finish_not_implemented_len = $-coroutine_finish_not_implemented
 ok: db "OK", 0, 10
 ok_len = $-ok
 
+; Current executing coroutine index
 contexts_current: dq 0
+; Points to the end of available stack space
 stacks_end:       dq stacks+COROUTINES_CAPACITY*STACK_CAPACITY
 
 section '.bss'
+; Stack space for all coroutines
 stacks:           rb COROUTINES_CAPACITY*STACK_CAPACITY
-contexts_rsp:     rq COROUTINES_CAPACITY
-contexts_rbp:     rq COROUTINES_CAPACITY
-contexts_rip:     rq COROUTINES_CAPACITY
+; Arrays storing context information for each coroutine
+contexts_rsp:     rq COROUTINES_CAPACITY  ; Stack pointers
+contexts_rbp:     rq COROUTINES_CAPACITY  ; Base pointers
+contexts_rip:     rq COROUTINES_CAPACITY  ; Instruction pointers
 public contexts_count
-contexts_count:   rq 1
+contexts_count:   rq 1                    ; Number of active coroutines
--- main.c
+++ main.c
@@ -1,23 +1,37 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void coroutine_init(void);
-void coroutine_go(void (*f)(void));
-void coroutine_yield(void);
+// Function declarations for our coroutine primitives implemented in assembly
+// These form the core API of our coroutine system
+void coroutine_init(void);                // Initialize the coroutine system
+void coroutine_go(void (*f)(void));       // Create and start a new coroutine
+void coroutine_yield(void);               // Yield execution to next coroutine
 
+// Example coroutine function that counts from 0 to 9
+// Each time it prints a number, it yields to allow other coroutines to run
 void counter(void)
 {
     for (int i = 0; i < 10; ++i) {
-        printf("%d\n", i);
-        coroutine_yield();
+        printf("%d\n", i);        // Print current count
+        coroutine_yield();        // Give up control to next coroutine
     }
 }
 
 int main()
 {
+    // Initialize the coroutine system
+    // This sets up the first coroutine (main)
     coroutine_init();
-    coroutine_go(counter);
-    coroutine_go(counter);
+
+    // Create two instances of the counter coroutine
+    // They will run concurrently, taking turns executing
+    coroutine_go(counter);        // First counter: will print 0,1,2,...,9
+    coroutine_go(counter);        // Second counter: will print 0,1,2,...,9
+
+    // Main loop - keep yielding forever
+    // This allows the counter coroutines to alternate execution
+    // The output will be interleaved: 0,0,1,1,2,2,...,9,9
     while (true) coroutine_yield();
-    return 0;
+
+    return 0;  // Never reached due to infinite loop above
 }
